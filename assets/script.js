a9e12600f0f271efeddc4292062bfc6992afd2fa
George <ghern5234@gmail.com>
2024-03-25 21:37:39 -0500

    made additions to the js script, added weather api

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+const APIKey = '0ddbdd01e0d6ab99523811f618c306be';
+const searchedEl = document.getElementByID('searched-cities');
+
+function init() {
+    const searchedLocations = JSON.parse(localStorage.getItems('locations'));
+
+    if (searchedLocations.length === 0) {
+        return;
+    }
+    
+    for (const location of searchedLocations) {
+
+    } 
+};
+
+init();
+
+
+
+
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
